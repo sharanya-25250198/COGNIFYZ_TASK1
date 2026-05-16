b6d2be70906f3224d6f27c38606f8868f43c4f70
sharanya-25250198 <25250198sharanya@staloysius.ac.in>
2026-05-16 22:13:09 +0530

    completed Task 5

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
 });
